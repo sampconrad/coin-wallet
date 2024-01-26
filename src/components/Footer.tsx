1e76a3d1132232629ecac7ffdde59c8c3ffaa74a
Conrado Sampaio <102433904+sampconrad@users.noreply.github.com>
2024-01-26 17:13:25 -0300

    fixed favicon & added link to portfolio

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,21 +3,29 @@ import CoingeckoLogo from '/coingecko.svg';
 
 export default function Footer() {
   return (
-    <div className='mt-auto flex items-center justify-center pt-5 pb-3 text-xs text-zinc-400 flex-wrap gap-x-1'>
-      <div className='flex items-center'>
-        {' '}
-        Powered by
-        <Avatar className='w-6 h-6 mx-1'>
-          <AvatarImage src={CoingeckoLogo} />
-        </Avatar>
+    <div className='mt-auto flex items-center justify-center pt-5 pb-3 text-xs text-zinc-400'>
+      <div className='flex flex-col items-center gap-1'>
+        <div className='flex items-center'>
+          {' '}
+          Powered by
+          <Avatar className='w-6 h-6 mx-1'>
+            <AvatarImage src={CoingeckoLogo} />
+          </Avatar>
+          <a
+            className='underline hover:text-blue-400 mr-1'
+            href='https://www.coingecko.com/'
+            target='_blank'>
+            CoinGecko.
+          </a>
+          API Rate: 30 hits/min.
+        </div>
         <a
-          className='underline text-blue-400'
-          href='https://www.coingecko.com/'
+          className='underline hover:text-blue-400 mr-1'
+          href='https://www.sampconrad.com/'
           target='_blank'>
-          CoinGecko.
+          Developed by sampconrad
         </a>
       </div>
-      API Rate Limit: 30 hits/min.
     </div>
   );
 }
